0f95d9f73cf60d7af7fee6a1c063ea5692d072ea
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-04 15:41:10 +0900

    Unity 게임 개발 입문 - 5강 캐릭터 이동 구현

--- Week1_TopDown_Shooting/Assets/Scripts/Controllers/TopDownCharacterController.cs
+++ Week1_TopDown_Shooting/Assets/Scripts/Controllers/TopDownCharacterController.cs
@@ -1,21 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TopDownCharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // event 외부에서는 호출하지 못하게 막든다
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    public void CallMoveEvent(Vector2 direction)
     {
-        
+        OnMoveEvent?.Invoke(direction);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CallLookEvent(Vector2 direction)
     {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
-
-        transform.position += new Vector3(x, y);
+        OnLookEvent?.Invoke(direction);
     }
 }
+
+// 이동 연습 코드
+//[SerializeField] private float speed = 5f;
+
+
+// Update is called once per frame
+//void Update()
+//{
+    //float x = Input.GetAxisRaw("Horizontal");
+    //float y = Input.GetAxisRaw("Vertical");
+
+    //transform.position += new Vector3(x, y) * speed * Time.deltaTime;
+//}
