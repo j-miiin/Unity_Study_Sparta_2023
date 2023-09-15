f87311eab8bbbb54314095d2ad0d567adcdc2c34
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-15 20:03:05 +0900

    Unity 게임 개발 숙련 - 9깅 스텟 만들기

--- Week1_TopDown_Shooting/Assets/Scripts/Controllers/TopDownCharacterController.cs
+++ Week1_TopDown_Shooting/Assets/Scripts/Controllers/TopDownCharacterController.cs
@@ -13,6 +13,13 @@ public class TopDownCharacterController : MonoBehaviour
     private float _timeSinceLastAttack = float.MaxValue;
     protected bool IsAttacking { get; set; }
 
+    protected CharacterStatsHandler Stats { get; private set; }
+
+    protected virtual void Awake()
+    {
+        Stats = GetComponent<CharacterStatsHandler>();
+    }
+
     protected virtual void Update()
     {
         HandleAttackDelay();
@@ -20,12 +27,14 @@ public class TopDownCharacterController : MonoBehaviour
 
     private void HandleAttackDelay()
     {
-        if (_timeSinceLastAttack <= 0.2f)
+        if (Stats.CurrentStats.attackSO == null) return;
+
+        if (_timeSinceLastAttack <= Stats.CurrentStats.attackSO.delay)
         {
             _timeSinceLastAttack += Time.deltaTime;
         }
 
-        if (IsAttacking && _timeSinceLastAttack > 0.2f)
+        if (IsAttacking && _timeSinceLastAttack > Stats.CurrentStats.attackSO.delay)
         {
             _timeSinceLastAttack = 0;
             CallAttackEvent();
