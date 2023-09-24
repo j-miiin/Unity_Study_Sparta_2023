c0afc3b70b950ee3385a75538da991a6e5f3b505
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-09-24 20:55:12 +0900

    Unity 게임 개발 숙련 - 30강 아이템 장착과 모션 수강

---
+++ Lecture_Survival_3D/Assets/Scripts/Player/EquipTool.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipTool : Equip
+{
+    public float attackRate;
+    private bool attacking;
+    public float attackDistance;
+
+    // 자원을 채취할 수 있는지
+    [Header("Resource Gathering")]
+    public bool doesGatherResources;
+
+    // 전투를 진행할 수 있는지
+    [Header("Combat")]
+    public bool doesDealDamage;
+    public int damage;
+
+    private Animator animator;
+    private Camera camera;
+
+    private void Awake()
+    {
+        camera = Camera.main;
+        animator = GetComponent<Animator>();
+    }
+
+    public override void OnAttackInput()
+    {
+        if (!attacking)
+        {
+            attacking = true;
+            animator.SetTrigger("Attack");
+            Invoke("OnCanAttack", attackRate);
+        }
+    }
+
+    void OnCanAttack()
+    {
+        attacking = false;
+    }
+}
